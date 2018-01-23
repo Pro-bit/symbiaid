eb12a0e0fd5b9ecac157718890c3e825d4b9b14d
Bor Motore <bor.motore@gmail.com>
2018-01-23 21:42:48 +0100

    sign up form fixes, loading spinner when waiting for transaction to finish

--- client/js/services/auth.js
+++ client/js/services/auth.js
@@ -1,44 +1,38 @@
-// Copyright IBM Corp. 2015. All Rights Reserved.
-// Node module: loopback-getting-started-intermediate
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 angular
   .module('app')
-  .service('AuthService', ['$q', '$rootScope', '$state', 'ContractService', function(
-      $q, $rootScope, $state, ContractService) {
+  .service('AuthService', ['$q', '$rootScope', '$state', 'ContractService', function (
+    $q, $rootScope, $state, ContractService) {
 
-    this.register = function(user) {
-      // return User
-      //   .create({
-      //    email: email,
-      //    password: password
-      //  })
-      //  .$promise;
-
+    this.register = function (user) {
       var def = $q.defer();
 
       // register with address to blockchain
-      var res = ContractService.getSYBContract().then(function(instance) {
-        instance.createUser(user.name, user.lastname, user.userAddress, function(res) {
-          def.resolve(res);
+      var res = ContractService.getSYBContract().then(function (instance) {
+        // when user is created, it should go to main app immediatelly
+        instance.onCreateUser({
+          userAddress: ContractService.web3.eth.coinbase
+        }, {
+            fromBlock: 0,
+            toBlock: 'latest'
+          }).watch(function (error, event) {
+            def.resolve(res);
+          });
+
+        // create new user on contract instance
+        instance.createUser(user.name, user.lastname, user.userAddress, function (res) {
+          // user created the watcher should be called
         });
       });
 
       return def.promise;
     }
 
-    this.isAuthenticated = function() {
+    this.isAuthenticated = function () {
       var def = $q.defer();
 
-      // register with address to blockchain
-      var res = ContractService.getSYBContract().then(function(instance) {
-        // instance.payServiceOrder(3, "0xc2dbc0a6b68d6148d80273ce4d6667477dbf2aa7", 5, function(err, result){
-        //   debugger;
-        //   def.resolve(!(_.isEmpty(result[0]) && _.isEmpty(result[1]) && _.isEmpty(result[2])));
-        // });
-
-        instance.users(ContractService.web3.eth.coinbase, function(err, result){
+      // check if user is already registered
+      var res = ContractService.getSYBContract().then(function (instance) {
+        instance.users(ContractService.web3.eth.coinbase, function (err, result) {
           def.resolve(!(_.isEmpty(result[0]) && _.isEmpty(result[1]) && _.isEmpty(result[2])));
         });
       });
