0297fa9f2495a420605a960b76a9aee2174a7951
Bor Motore <bor.motore@gmail.com>
2018-01-24 14:15:58 +0100

    App - filters, edit, create
    App - filters, edit, create

--- client/js/controllers/aid-services.js
+++ client/js/controllers/aid-services.js
@@ -1,7 +1,8 @@
 angular
     .module('app')
-    .controller('AidServicesController', ['$scope', 'SYBService', 'ContractService', function ($scope, SYBService, ContractService) {
+    .controller('AidServicesController', ['$scope', 'SYBService', 'ContractService', function ($scope, SYBService, ContractService){
         $scope.SYBServices = [];
+        $scope.userId = ContractService.web3.eth.coinbase;
 
         SYBService
             .find()
@@ -11,29 +12,110 @@ angular
             });
 
         // add some fake services
+        /*
         SYBService
             .create({
                 Name: "Servis " + $scope.SYBServices.length,
                 Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + $scope.SYBServices.length,
                 Price:  + $scope.SYBServices.length,
                 UserCreated: ContractService.web3.eth.coinbase,
-                Category: Math.floor(Math.random() * 5)
+                ServiceType: Math.floor(Math.random() * 5)
             })
             .$promise
             .then(function(res) {
                 console.log("added", res);
             });
+        */
 
 
-        //$scope.categoryFilter = 0;
+        $scope.showEdit = function(userId){
+            return $scope.userId == userId?true:false;
+        }
 
-        $scope.readMore = function(service){
-            $scope.selectedService = service;
-            $('.tiny.modal').modal('show');
+        $scope.addEditService = function(id){
+            
+            if(id){
+                SYBService.findById({ id: id })
+                    .$promise
+                    .then(function(data) {
+                        $scope.service = data;
+                });
+            }else{
+                $scope.service = {
+                    UserCreated: $scope.userId,
+                    ServiceType: 0
+                }; 
+            }
+
+
+            $scope.serviceTypes = [
+                {
+                    Id: 0,
+                    Name: "House work"
+                },
+                {
+                    Id: 1,
+                    Name: "Landscaping"
+                },
+                {
+                    Id: 2,
+                    Name: "Socializing"
+                },
+                {
+                    Id: 3,
+                    Name: "Food"
+                },
+                {
+                    Id: 4,
+                    Name: "Transport"
+                },
+            ];
+
+            $scope.submitForm = function () {
+
+                if(id){
+                    $scope.service
+                        .$save()
+                        .then(function () {
+                            SYBService.find().$promise.then(function (SYBService) {
+                                $scope.SYBServices = SYBService;
+                            });
+                        });
+                }else{
+                    SYBService
+                        .create($scope.service)
+                        .$promise
+                        .then(function () {
+                            SYBService.find().$promise.then(function (SYBService) {
+                                $scope.SYBServices = SYBService;
+                            });
+                        });
+                }
+                $('.tiny.modal.addService').modal('hide'); 
+            };
+
+            $scope.discard = function () {
+                $('.tiny.modal.addService').modal('hide'); 
+            };
+
+            $('.tiny.modal.addService').modal('show');
         }
 
 
-        $scope.getCategoryIcon = function(category){
+
+        $scope.readMore = function(service){
+            $scope.selectedService = service;
+            $('.tiny.modal.showMore').modal('show');
+        }
+
+        $scope.buyService = function(service){
+            ContractService.buy(service).then(function(res){
+                debugger;
+            })
+        }
+
+
+        $scope.getServiceTypeIcon = function(category){
             var icon = ""
             switch(category){
                 case 0:
--- client/js/services/contract-services.js
+++ client/js/services/contract-services.js
@@ -1,7 +1,7 @@
 angular
 	.module('app')
-	.service('ContractService', ['$q', '$rootScope', '$state', '$http', function (
-		$q, $rootScope, $state, $http) {
+	.service('ContractService', ['$q', '$rootScope', '$state', '$http', 'SYBService', function (
+		$q, $rootScope, $state, $http, SYBService) {
 
 		// get metamask address
 		if (typeof web3 !== 'undefined') {
@@ -24,11 +24,63 @@ angular
 				$http.get('SYBContract.json').success(function (response) {
 					this.contractJson = response;
 
-					this.SYBContract = this.web3.eth.contract(this.contractJson.abi).at("0x1dFFEA5b11C389986C6ceb65090bf3dF300EC9bC");
+					this.SYBContract = this.web3.eth.contract(this.contractJson.abi).at("0xDcE03A21f544353C032D4686B1Ba06E063292Eb5");
 
 					def.resolve(this.SYBContract);
 				});
 			}
+
+			return def.promise;
+		}
+
+
+		this.buy = function(service){
+					//delete&create new
+					SYBService
+				      .deleteById({ id: service.id })
+				      .$promise
+				      .then(function(res) {
+				      		var newService = service;
+				      		delete newService["id"];
+				            SYBService
+		                        .create(newService)
+		                        .$promise
+		                        .then(function () {
+		                        	debugger;
+		                        	/*
+		                            SYBService.find().$promise.then(function (SYBService) {
+		                                $scope.SYBServices = SYBService;
+		                            });
+		                            */
+		                        });
+				      });
+
+			var def = $q.defer();
+
+			// register with address to blockchain
+			var res = this.getSYBContract().then(function(instance){
+
+				/*
+				instance.onCreateUser({
+					userAddress: ContractService.web3.eth.coinbase
+				}, {
+					fromBlock: 0,
+					toBlock: 'latest'
+				}).watch(function (error, event) {
+
+
+				});
+				*/
+
+				/*
+				instance.createServiceOrder(service.id, service.ServiceType, service.UserCreated, service.Name, service.Description, service.Price, function(err,res){
+
+					debugger;
+
+				})
+				*/
+
+			});
 
 			return def.promise;
 		}
