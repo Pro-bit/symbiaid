b44cc0576f1ee9e81a4498cb4eacbc1035c5345f
Andrej Kostic <kostic.andrej@hotmail.com>
2018-01-24 09:37:14 +0100

    fixed merge conflicts

--- client/js/controllers/aid-services.js
+++ client/js/controllers/aid-services.js
@@ -98,38 +98,61 @@ angular
             },
         ];
 
+        $scope.submitForm = function () {
+            SYBService
+                .create($scope.service)
+                .$promise
+                .then(function () {
+                    $state.go('index');
+                });
+        };
 
-        //$scope.categoryFilter = 0;
+        $scope.discard = function () {
+            $state.go("index");
+        };
+    }])
+    .controller('EditServiceController', ['$scope', '$q', '$stateParams', '$state', 'SYBService', function ($scope, $q, $stateParams, $state, SYBService) {
+            $scope.action = 'Edit';
+            $scope.service = {};
 
-        $scope.readMore = function(service){
-            $scope.selectedService = service;
-            $('.tiny.modal').modal('show');
-        }
+            $scope.serviceTypes = [
+                {
+                    Id: 0,
+                    Name: "House work"
+                },
+                {
+                    Id: 1,
+                    Name: "Socializing"
+                },
+                {
+                    Id: 2,
+                    Name: "afdfas"
+                },
+                {
+                    Id: 3,
+                    Name: "Food transport"
+                },
+                {
+                    Id: 4,
+                    Name: "House work"
+                },
+            ];
 
+            SYBService.findById({ id: $stateParams.id })
+                .$promise
+                .then(function(data) {
+                    $scope.service = data;
+                });
 
-        $scope.getCategoryIcon = function(category){
-            var icon = ""
-            switch(category){
-                case 0:
-                    icon = "home"
-                    break
-                case 1:
-                    icon = "tree"
-                    break
-                case 2:
-                    icon = "comment outline"
-                    break
-                case 3:
-                    icon = "food"
-                    break
-                case 4:
-                    icon = "car"
-                    break
-                case 5:
-                    icon = "grid layout"
-                    break
-            }
-
-            return icon
-        }
-}]);
+            $scope.submitForm = function () {
+                $scope.service
+                    .$save()
+                    .then(function () {
+                        $state.go('index');
+                    });
+            };
+    
+            $scope.discard = function () {
+                $state.go("index");
+            };
+        }]);
