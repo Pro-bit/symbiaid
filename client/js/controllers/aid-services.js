8f76d9bb6a44edb1c3d3e66608c094dd2e5c437f
Bor Motore <bor.motore@gmail.com>
2018-01-24 09:22:32 +0100

    App design update
    App design update

--- client/js/controllers/aid-services.js
+++ client/js/controllers/aid-services.js
@@ -14,7 +14,7 @@ angular
         SYBService
             .create({
                 Name: "Servis " + $scope.SYBServices.length,
-                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." + $scope.SYBServices.length,
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + $scope.SYBServices.length,
                 Price:  + $scope.SYBServices.length,
                 UserCreated: ContractService.web3.eth.coinbase,
                 Category: Math.floor(Math.random() * 5)
@@ -50,6 +50,9 @@ angular
                     break
                 case 4:
                     icon = "car"
+                    break
+                case 5:
+                    icon = "grid layout"
                     break
             }
 
