8daa17b40f16832d7bb7967090dfa36d5e7bec4c
Bor Motore <bor.motore@gmail.com>
2018-01-24 08:09:55 +0100

    Presentation page + basic app
    Presentation page + basic app

--- client/js/controllers/aid-services.js
+++ client/js/controllers/aid-services.js
@@ -18,7 +18,8 @@ angular
             Name: "Servis " + $scope.SYBServices.length,
             Description: "krneki " + $scope.SYBServices.length,
             Price:  + $scope.SYBServices.length,
-            UserCreated: ContractService.web3.eth.coinbase
+            UserCreated: ContractService.web3.eth.coinbase,
+            Category: Math.floor(Math.random() * 4)
             })
             .$promise
             .then(function(res) {
@@ -29,13 +30,42 @@ angular
         SYBService
             .create({
                 Name: "Servis " + $scope.SYBServices.length,
-                Description: "krneki " + $scope.SYBServices.length,
+                Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." + $scope.SYBServices.length,
                 Price:  + $scope.SYBServices.length,
-                UserCreated: ContractService.web3.eth.coinbase
+                UserCreated: ContractService.web3.eth.coinbase,
+                Category: Math.floor(Math.random() * 4)
             })
             .$promise
             .then(function(res) {
                 console.log("added", res);
             });
+        }
+
+        //$scope.categoryFilter = 0;
+
+        $scope.readMore = function(service){
+            $scope.selectedService = service;
+            $('.tiny.modal').modal('show');
+        }
+
+
+        $scope.getCategoryIcon = function(category){
+            var icon = ""
+            switch(category){
+                case 0:
+                    icon = "tree"
+                    break
+                case 1:
+                    icon = "comment outline"
+                    break
+                case 2:
+                    icon = "food"
+                    break
+                case 3:
+                    icon = "car"
+                    break
+            }
+
+            return icon
         }
 }]);
